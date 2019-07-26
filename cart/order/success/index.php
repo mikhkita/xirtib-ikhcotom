634eb30d8c86cb96dd6d413e89f8c796d5037719
Китаев Михаил <beatbox787@gmail.com>
2019-07-26 19:25:17 +0700

    no message

---
+++ cart/order/success/index.php
@@ -0,0 +1,16 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оформление заказа");
+
+$orderID = $_REQUEST["ORDER_ID"];
+
+?>
+<div class="b-block">
+	<?if($orderID > 0):?>
+		<h2 class="b-title">Ваш заказ №<?=$orderID?> успешно оформлен</h2>
+	<?else:?>
+		<h2 class="b-title">Не удалось создать заказ</h2>
+	<?endif;?>
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
