68f92a45ff4b23da230d63c870f8d7b074cfe9e3
Дмитрий Муленок <mulenokd@gmail.com>
2019-07-26 18:59:03 +0700

    no message

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -6,6 +6,17 @@ function vardump($str){
 	echo "</pre>";
 }
 
+function randomPassword() {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array();
+    $alphaLength = strlen($alphabet) - 1;
+    for ($i = 0; $i < 8; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass);
+}
+
 function getBasketCount(){
 	CModule::IncludeModule("sale");
 
