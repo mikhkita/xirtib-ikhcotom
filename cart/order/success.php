c2e563685d11042231d660f6bf8abb3039e093f5
Китаев Михаил <beatbox787@gmail.com>
2019-07-27 16:57:54 +0700

    no message

--- cart/order/success.php
+++ cart/order/success.php
@@ -23,7 +23,9 @@ $APPLICATION->SetTitle("Оформление заказа");
 	$email = $_REQUEST["email"];
 	$address = $_REQUEST["address"];
 	$comment = $_REQUEST["comment"];
-	$payment = $_REQUEST["payment"];
+	$paymentId = $_REQUEST["payment"];
+	$deliveryId = $_REQUEST["delivery"];
+	$deliveryPrice = $_REQUEST["delivery-cost"];
 
 	$siteId = Context::getCurrent()->getSite();
 	$currencyCode = CurrencyManager::getBaseCurrency();
@@ -47,22 +49,26 @@ $APPLICATION->SetTitle("Оформление заказа");
 			\Bitrix\Sale\Fuser::getId());
 		$order->setBasket($basket);
 
-		// Создаём одну отгрузку и устанавливаем способ доставки - "Без доставки" (он служебный)
-		// $shipmentCollection = $order->getShipmentCollection();
-		// $shipment = $shipmentCollection->createItem();
-		// $service = Delivery\Services\Manager::getById(Delivery\Services\EmptyDeliveryService::getEmptyDeliveryServiceId());
-		// $shipment->setFields(array(
-		//     'DELIVERY_ID' => $service['ID'],
-		//     'DELIVERY_NAME' => $service['NAME'],
-		// ));
-		// $shipmentItemCollection = $shipment->getShipmentItemCollection();
-		// $shipmentItem = $shipmentItemCollection->createItem($item);
-		// $shipmentItem->setQuantity($item->getQuantity());
+		// Способ доставки
+		$shipmentCollection = $order->getShipmentCollection();
+		$shipment = $shipmentCollection->createItem();
+		$service = Delivery\Services\Manager::getById($deliveryId);
+		$shipment->setFields(array(
+		    'DELIVERY_ID' => $service['ID'],
+		    'DELIVERY_NAME' => $service['NAME'],
+		    'PRICE_DELIVERY' => $deliveryPrice,
+    		'BASE_PRICE_DELIVERY' => $deliveryPrice,
+		));
+		$shipmentItemCollection = $shipment->getShipmentItemCollection();
+		foreach ($basket as $key => $item) {
+			$shipmentItem = $shipmentItemCollection->createItem($item);
+			$shipmentItem->setQuantity($item->getQuantity());
+		}
 
 		// Способ оплаты
 		$paymentCollection = $order->getPaymentCollection();
 		$paymentItem = $paymentCollection->createItem();
-		$paySystemService = \Bitrix\Sale\PaySystem\Manager::getObjectById($payment);
+		$paySystemService = \Bitrix\Sale\PaySystem\Manager::getObjectById($paymentId);
 		$paymentItem->setFields(array(
 		    'PAY_SYSTEM_ID' => $paySystemService->getField("ID"),
 		    'PAY_SYSTEM_NAME' => $paySystemService->getField("NAME"),
--- cart/order/success/index.php
+++ cart/order/success/index.php
@@ -7,22 +7,34 @@ use Bitrix\Sale;
 
 $order = Sale\Order::load($_REQUEST["ORDER_ID"]);
 
-if( !$order || $order->getUserId() != $USER->GetID() ){
+if( !$order || $order->getUserId() != $USER->GetID() || !isset($_REQUEST["ORDER_ID"]) ){
 	LocalRedirect("/cart/order/");
 }
 
-// var_dump($order);
+$paymentId = array_pop($order->getPaymentSystemId());
+$payment = \Bitrix\Sale\PaySystem\Manager::getById($paymentId);
+
+$deliveryId = array_pop($order->getDeliverySystemId());
+$delivery = \Bitrix\Sale\Delivery\Services\Manager::getById($deliveryId);
+
+// var_dump($payment);
 
 
 
 ?>
-<div class="b-block b-text b-politics-text">
+<div class="b-text">
 	<? if( $order->isPaid() ): ?>
-		<h1 class="b-title">Ваш заказ №<?=$orderID?> успешно создан!</h1>
-		<p>Наш менеджер свяжется с Вами в ближайшее время по телефону, который Вы указали  при оформлении заказа, для уточнения деталей.</p>
+		<h1 class="b-title b-politics-text">Ваш заказ №<?=$order->getId()?> успешно оплачен</h1>
+		<p style="max-width: 600px;">Оплата заказа прошла успешно! Наш менеджер свяжется с Вами в ближайшее время по телефону, который Вы указали при оформлении заказа, для уточнения деталей.</p>
 	<? else: ?>
-		<h1 class="b-title b-politics-text">Ваш заказ №<?=$orderID?> успешно оплачен</h1>
-		<p>Оплата заказа прошла успешно! Наш менеджер свяжется с Вами в ближайшее время по телефону, который Вы указали при оформлении заказа, для уточнения деталей.</p>
+		<h1 class="b-title">Ваш заказ №<?=$order->getId()?> успешно создан!</h1>
+		<p style="max-width: 600px;">Наш менеджер свяжется с Вами в ближайшее время по телефону, который Вы указали  при оформлении заказа, для уточнения деталей.</p>
+		<ul>
+			<li><b>Способ доставки:</b> <?=$delivery["NAME"]?></li>
+			<li><b>Способ оплаты:</b> <?=$payment["NAME"]?></li>
+			<li><b>Стоимость доставки:</b> <?=$order->getDeliveryPrice()?> руб.</li>
+			<li><b>Сумма к оплате:</b> <?=$order->getPrice()?> руб.</li>
+		</ul>
 	<? endif; ?>
 </div>
 
