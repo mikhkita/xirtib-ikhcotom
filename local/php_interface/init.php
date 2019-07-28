18afd48e9d0556dae6337fda4d30307c050d5234
Китаев Михаил <beatbox787@gmail.com>
2019-07-28 16:21:09 +0700

    r

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,5 +1,16 @@
 <?
 
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+
+$config = require __DIR__ . '/../../config/web.php';
+
+(new yii\web\Application($config))->run();
+
+
 function vardump($str){
 	echo "<pre style='text-align:left;'>";
 	var_dump($str);
