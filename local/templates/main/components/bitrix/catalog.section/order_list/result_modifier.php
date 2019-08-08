bf9643facff8a30a08521452868553f1fa54426b
Дмитрий Муленок <mulenokd@gmail.com>
2019-08-08 21:30:47 +0700

    no message

--- local/templates/main/components/bitrix/catalog.section/order_list/result_modifier.php
+++ local/templates/main/components/bitrix/catalog.section/order_list/result_modifier.php
@@ -1,14 +1,32 @@
 <?
 
+$temp = null; //первая фотка
+
 foreach ($arResult['ITEMS'] as $k => $arItem) {
-	
-	foreach ($arResult['ITEMS'][$k]["OFFERS"] as $i => $offer) {
-		$quantity = intval($offer["PRODUCT"]["QUANTITY"]);
-		if( $quantity <= 0 ){
-			unset($arResult['ITEMS'][$k]["OFFERS"][$i]);
+
+	if ($arResult['ITEMS'][$k]["OFFERS"]) {
+		foreach ($arResult['ITEMS'][$k]["OFFERS"] as $i => $offer) {
+			if ($temp == null && $offer['DETAIL_PICTURE']) {
+				$temp = $offer['DETAIL_PICTURE'];
+			}
+			$quantity = intval($offer["PRODUCT"]["QUANTITY"]);
+			if( $quantity <= 0 ){
+				unset($arResult['ITEMS'][$k]["OFFERS"][$i]);
+			}
 		}
+
+		if (empty($arResult['ITEMS'][$k]["OFFERS"]) && ($temp != null)) {
+			$arResult['ITEMS'][$k]['DETAIL_PICTURE'] = $temp;
+		}
+
 	}
 
+	if (!empty($arResult['ITEMS'][$k]["OFFERS"])) {
+		$arResult['ITEMS'][$k]["OFFERS"] = array_values($arResult['ITEMS'][$k]["OFFERS"]);
+	}
+	
 }
+
+$arResult['ITEMS'] = array_values($arResult['ITEMS']);
 
 ?>
--- local/templates/main/components/bitrix/catalog.section/slider/result_modifier.php
+++ local/templates/main/components/bitrix/catalog.section/slider/result_modifier.php
@@ -1,14 +1,29 @@
 <?
 
 foreach ($arResult['ITEMS'] as $k => $arItem) {
-	
-	foreach ($arResult['ITEMS'][$k]["OFFERS"] as $i => $offer) {
-		$quantity = intval($offer["PRODUCT"]["QUANTITY"]);
-		if( $quantity <= 0 ){
-			unset($arResult['ITEMS'][$k]["OFFERS"][$i]);
+
+	if ($arResult['ITEMS'][$k]["OFFERS"]) {
+		foreach ($arResult['ITEMS'][$k]["OFFERS"] as $i => $offer) {
+			$quantity = intval($offer["PRODUCT"]["QUANTITY"]);
+			if( $quantity <= 0 ){
+				unset($arResult['ITEMS'][$k]["OFFERS"][$i]);
+			}
 		}
+		if (empty($arResult['ITEMS'][$k]["OFFERS"])) {
+			unset($arResult['ITEMS'][$k]);
+		}
+	} else {
+		$quantity = intval($arResult['ITEMS'][$k]["PRODUCT"]['QUANTITY']);
+		if( $quantity <= 0 ){
+			unset($arResult['ITEMS'][$k]);
+		}
+	}
+	if (!empty($arResult['ITEMS'][$k]["OFFERS"])) {
+		$arResult['ITEMS'][$k]["OFFERS"] = array_values($arResult['ITEMS'][$k]["OFFERS"]);
 	}
 	
 }
+
+$arResult['ITEMS'] = array_values($arResult['ITEMS']);
 
 ?>
