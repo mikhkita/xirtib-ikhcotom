3b2b6c740c4ce97017ea6ab6bc7f6f1ea77f8279
Китаев Михаил <beatbox787@gmail.com>
2019-08-16 16:27:27 +0700

    no message

--- local/templates/main/components/bitrix/catalog.section/main/template.php
+++ local/templates/main/components/bitrix/catalog.section/main/template.php
@@ -61,8 +61,12 @@ if(count($arResult["ITEMS"])): ?>
 				<? $discountPrice = convertPrice($arItem["PRICES"]["PRICE"]["DISCOUNT_VALUE"]); ?>
 
 			<? endif; ?>
+			<?
+			$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+			?>
 
-			<div class="b-item-card">
+			<div class="b-item-card" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="b-card-hover-frame"></a>
 				<div class="b-card-top">
 					<? 
