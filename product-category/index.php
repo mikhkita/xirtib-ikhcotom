e73c1f763598bdb1c7afafa2105539a1501175f2
Китаев Михаил <beatbox787@gmail.com>
2019-08-27 11:29:55 +0700

    no message

--- product-category/index.php
+++ product-category/index.php
@@ -2,9 +2,11 @@
 
 $GLOBALS['APPLICATION']->RestartBuffer();
 // vardump($urlArr[2]);
-$url = '/catalog/'.$urlArr[2].'/';
-echo '<script>window.location = "'.$url.'";</script>';
-// header('Location: /catalog/'.$urlArr[2].'/');
+// $url = '/catalog/'.$urlArr[2].'/';
+// echo '<script>window.location = "'.$url.'";</script>';
+header("HTTP/1.1 301 Moved Permanently");
+header('Location: http://motochki-klubochki.ru/catalog/'.$urlArr[2].'/');
+die();
 // vardump('ok');
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
