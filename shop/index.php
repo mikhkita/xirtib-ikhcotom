b58b10497ece3b40fe295ca80001ccf7084573c1
Дмитрий Муленок <mulenokd@gmail.com>
2019-08-27 12:48:25 +0700

    no message

--- shop/index.php
+++
@@ -1,6 +0,0 @@
-<? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-
-$GLOBALS['APPLICATION']->RestartBuffer();
-echo '<script>window.location = "/catalog/";</script>';
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
